0655bb99a9db980eee2949ce1011e8af12805e00
Moncef Belyamani <giter@done>
2013-03-29 00:01:41 -0700

    rails_apps_composer: set email accounts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,4 +61,21 @@ HumanServicesFinder::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'example.com' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"  
+
+    config.action_mailer.smtp_settings = {
+      :address   => "smtp.mandrillapp.com",
+      :port      => 25,
+      :user_name => ENV["MANDRILL_USERNAME"],
+      :password  => ENV["MANDRILL_API_KEY"]
+    }
+
+
+
 end
