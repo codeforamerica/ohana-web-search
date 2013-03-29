9597376160bb45933e1fba3c954b124b92046f90
Moncef Belyamani <giter@done>
2013-03-29 00:01:47 -0700

    rails_apps_composer: models

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User
+  include Mongoid::Document
+  rolify
+  field :provider, type: String
+  field :uid, type: String
+  field :name, type: String
+  field :email, type: String
+  attr_accessible :role_ids, :as => :admin
+  attr_accessible :provider, :uid, :name, :email
+  validates_presence_of :name
+  # run 'rake db:mongoid:create_indexes' to create indexes
+  index({ email: 1 }, { unique: true, background: true })
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+         user.name = auth['info']['name'] || ""
+         user.email = auth['info']['email'] || ""
+      end
+    end
+  end
+
+end
