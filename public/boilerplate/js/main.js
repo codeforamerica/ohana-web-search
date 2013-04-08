7760759665f4687dbce8b7158bc9de116ef5d799
Anselm <ans@anselmbradford.com>
2013-04-08 14:48:25 -0700

    Set up loading of mock JSON data source

--- public/boilerplate/js/main.js
+++ public/boilerplate/js/main.js
@@ -1,1 +1,40 @@
+var main = (function () {
+"use strict";
 
+	var main = {};
+	
+	var JSON; // loaded data
+
+	// initalize the application
+	main.init = function()
+	{
+		var path = "data/dbmock.json"; // load mock data
+		$.getJSON(path, function(response){
+		   JSON = response;
+		})
+		
+		.success(function() { dataLoadedSuccess(); })
+		.error(function() { dataLoadedError(); })
+		.complete(function() { dataLoadedComplete(); });
+	}
+
+	// functions for JSON loading success
+	function dataLoadedSuccess()
+	{
+		console.log("data loaded successful!");
+	}
+	
+	function dataLoadedError()
+	{
+		console.log("error loading data!");
+	}
+	
+	function dataLoadedComplete()
+	{
+		console.log("completed loading of data!");
+	}
+
+// return internally scoped var as value of globally scoped object
+return main;
+
+})();
