c3325ce35c7f6268869e06fe86c0be9d7c9acfa8
Moncef Belyamani <giter@done>
2013-04-23 16:37:33 -0700

    adds test for address method in organization model

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Organization do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @org = Organization.new(:name => "EHP", :street_address => "1415 Pulgas Ave", :city => "East Palo Alto",
+    												:state => "CA", :zipcode => "94103", :phone => "350-657-9034")
+  end
+	
+	it "joins all address elements into one string" do
+		@org.address.should == "#{@org.street_address}, #{@org.city}, #{@org.state} #{@org.zipcode}"
+	end
+	
 end
