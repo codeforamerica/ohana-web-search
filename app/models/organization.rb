fdb4d9a44c8cb731f2312d88ad6f635818a09144
Moncef Belyamani <giter@done>
2013-04-23 14:58:44 -0700

    adds geocoder config to organization model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -9,6 +9,7 @@ class Organization
   field :url, type: String
   field :email, type: String
   field :phone, type: String
+  field :coordinates, type: Array
   field :latitude, type: Float
   field :longitude, type: Float
   field :business_hours, type: Hash
@@ -16,4 +17,12 @@ class Organization
 
   validates_presence_of :name, :street_address, :city, :state, :zipcode, :phone
   validates :zipcode, :length => { :minimum => 5, :maximum => 10 }
+
+  def address
+    "#{self.street_address}, #{self.city}, #{self.state} #{self.zipcode}"
+  end
+
+  include Geocoder::Model::Mongoid
+  geocoded_by :address               # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
 end
