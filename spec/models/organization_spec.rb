ce8a35cf11c276d933e74c64417f1869d52ff386
Moncef Belyamani <giter@done>
2013-04-23 17:52:37 -0700

    refactors organization tests using subject

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe Organization do
-  before :each do
-    @org = Organization.new(:name => "EHP", :street_address => "1415 Pulgas Ave", :city => "East Palo Alto",
-    												:state => "CA", :zipcode => "94103", :phone => "350-657-9034")
-  end
-	
+	it "responds to address" do
+		subject.should respond_to(:address)
+	end
+
 	it "joins all address elements into one string" do
-		@org.address.should == "#{@org.street_address}, #{@org.city}, #{@org.state} #{@org.zipcode}"
+		subject.address.should == "#{subject.street_address}, #{subject.city}, #{subject.state} #{subject.zipcode}"
 	end
 	
 end
