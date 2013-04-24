d33c62a91ff2c8c65a70ba34221c55357883ea8e
Moncef Belyamani <giter@done>
2013-04-23 22:06:41 -0700

    cleans up spec with concise syntax & uses FactoryGirl to build instance with dummy data

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe Organization do
-	it "responds to address" do
-		subject.should respond_to(:address)
-	end
-
-	it "joins all address elements into one string" do
-		subject.address.should == "#{subject.street_address}, #{subject.city}, #{subject.state} #{subject.zipcode}"
-	end
 	
+	subject { FactoryGirl.build(:organization) }
+
+	it { should respond_to(:address) }
+	
+	its(:address) { should == "#{subject.street_address}, #{subject.city}, #{subject.state} #{subject.zipcode}" }
+
 end
