1240a6ac68bc4585b36648bacd4187464d0a0b0f
Anselm <git@anselmbradford.com>
2013-04-30 15:33:52 -0400

    Added initial js structure

---
+++ app/assets/javascripts/initializer.js
@@ -0,0 +1,6 @@
+// Sets up script initialization based on page loading events.
+
+// run scripts when DOM has fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+	main.init(); // main initialization of custom script happens here
+}); 
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,38 @@
+/* Application functionality */
+
+var main = (function () {
+"use strict";
+	var main = {};
+
+	// initalize the application
+	main.init = function()
+	{
+		console.log("Hi! main.js has successfully loaded!");
+	}
+
+
+// return internally scoped var as value of globally scoped object
+return main;
+
+})();
+
+	
+
+
+/*
+// new app object template
+var search = (function () {
+"use strict";
+
+	var search = {};
+
+	// initalize the application
+	search.init = function()
+	{
+		
+	}
+
+	return search;
+
+})();
+*/
