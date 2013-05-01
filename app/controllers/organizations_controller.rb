78867abad667401f554d54294f23217475db881d
Moncef Belyamani <giter@done>
2013-04-30 21:47:17 -0400

    Let user select distance via drop-down on home page
    https://app.asana.com/0/5189012931969/5189012931958
    
    The distanoce was initially hard-coded to 5 miles, and the user was
    not able to select a different distance.
    
    * Determine the distance in the controller via the params[:miles]
      passed by the select tag on the home page.
    
    * Use an if statement to display search results based on whether or
      not an address was passed

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,9 +1,15 @@
 class OrganizationsController < ApplicationController
 	def index
-		@organizations = Organization.all
-		address = params[:address]
-	  @locations = Organization.near(address, 5)
-    @results_count = "#{@locations.size} results within 5 miles of #{address}"
+		params[:miles] = 5 if params[:miles].blank?
+    
+    if params[:address].present?
+			address = params[:address]
+		  @locations = Organization.near(address, params[:miles])
+	    @results_count = "#{@locations.size} results within #{params[:miles]} miles of #{address}"
+	  else
+	  	@locations = Organization.all.sort_by(&:name)
+	  	@results_count = "#{@locations.size} total results"
+	  end
 	end
 
 	def show
