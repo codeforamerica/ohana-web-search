58365a9ca9b08113b891bd6ecdb1d293c1dc39d7
Moncef Belyamani <giter@done>
2013-05-10 03:11:46 -0700

    Add distance from search results to search location. For #5
    The geocoder gem comes with a distance_to method, so I just needed to make the search location an instance variable in the controller so it can be used in the view.
    
    I also added an example use of the distance_to method in the view.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,12 +3,12 @@ class OrganizationsController < ApplicationController
 	def index
 		params[:miles] = 2 if params[:miles].blank?
     
-    keyword, location, radius = params["search-term"], params[:location], params[:miles]
+    keyword, @location, radius = params["search-term"], params[:location], params[:miles]
 
-		if Organization.query_invalid?(location)
+		if Organization.query_invalid?(@location)
 			redirect_to root_url, :alert => 'Please enter a full address or a valid 5-digit ZIP code.'
 		else
-			@locations, @results_text = Organization.find_by_keyword_and_location(keyword, location, radius)
+			@organizations, @results_text = Organization.find_by_keyword_and_location(keyword, @location, radius)
 		end
 	end
 
