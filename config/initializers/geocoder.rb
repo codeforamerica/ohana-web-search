5399d769623e1ab09a426002ee78fc691e3c1f8f
Moncef Belyamani <giter@done>
2013-05-15 16:58:37 -0700

    Add Redis config to geocoder gem so it can use REDIS constant
    The problem with Redis trying to connect to localhost on Heroku was because the geocoder cache config was pointing to Redis.new, which means localhost. By using the REDIS constant, it will point either to the production URL in production, or to localhost in the dev environment.
    
    Thanks to @ahhrrr for pointing me in the right direction!

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,1 +1,9 @@
-Geocoder.configure(:lookup => :mapquest, :cache => Redis.new, :always_raise => [Geocoder::OverQueryLimitError, Geocoder::RequestDenied, Geocoder::InvalidRequest, Geocoder::InvalidApiKey])
+require "redis"
+
+if Rails.env.production?
+  REDIS = Redis.connect(url: ENV["REDISTOGO_URL"])
+else
+  REDIS = Redis.new
+end
+
+Geocoder.configure(:lookup => :mapquest, :cache => REDIS, :always_raise => [Geocoder::OverQueryLimitError, Geocoder::RequestDenied, Geocoder::InvalidRequest, Geocoder::InvalidApiKey])
