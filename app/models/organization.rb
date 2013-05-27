f5a8ba20cd63409b4c118f5032c173d6824138c6
Anselm Bradford <git@anselmbradford.com>
2013-05-27 14:33:53 -0700

    Changes regex to match word boundary
    The regex for matching "th" in a name is changed to a word boundary
    after the "th" to handle, for instance, "25th " and "25th,"

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -55,7 +55,7 @@ class Organization
 
   def formatted_name
     if name.present?
-      fname = name.split(/(^.*\d)(th)(\s.*)/)
+      fname = name.split(/(^.*\d)(th)(\b.*)/)
       return fname
     end
   end 
