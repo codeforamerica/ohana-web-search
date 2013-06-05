f0df67cefb43ace0573510cbf884c5f484fe578a
Anselm Bradford <git@anselmbradford.com>
2013-06-05 14:59:59 -0700

    Implements superscript helper and test
    Adds implementation for superscript helper method for the 'th' ordinal
    and adds tests

--- app/helpers/superscript_format_helper.rb
+++ app/helpers/superscript_format_helper.rb
@@ -1,7 +1,20 @@
 module SuperscriptFormatHelper
-	# Format phone number as (XXX) XXX-XXXX
-	def superscript_ordinals(string)
-      fname = string.split(/(^.*\d)(th)(\b.*)/)
-	end
 
+	# Adds <sup>XX</sup> around ordinals
+	def superscript_ordinals(string)
+		fname = string.split(/(^.*\d)(th)(\b.*)/)
+
+		parsed = ''
+		fname.each do |snippet|
+						
+			if snippet == 'th'
+				snippet = '<>th</sup>'
+			end
+
+			parsed += snippet
+
+		end
+
+		parsed
+	end
 end
--- spec/helpers/superscript_format_helper_spec.rb
+++ spec/helpers/superscript_format_helper_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 describe SuperscriptFormatHelper do
+
+	it 'check for string beginning with a single "th" ordinal'  do 
+		expect { superscript_ordinals("4th year").should == "4<sup>th</sup>" }
+	end
 	
+	it 'check for string containing single "th" ordinal'  do 
+		expect { superscript_ordinals("the 25th year").should == "the 25<sup>th</sup> year" }
+	end
+
+	it 'check for string containing two "th" ordinal'  do 
+		expect { superscript_ordinals("the 25th and 26th year").should == "the 25<sup>th</sup> and 26<sup>th</sup> year" }
+	end
 	
 end
