054ba49561a8055b0d87cefb66f9b0a1bb9c2076
Anselm Bradford <git@anselmbradford.com>
2013-06-05 13:41:28 -0700

    Adds search endpoint methods and tests
    Adds methods and tests for Ohana API 'search' endpoint

--- lib/ohanakapa/wrapper.rb
+++ lib/ohanakapa/wrapper.rb
@@ -5,5 +5,14 @@ module Ohanakapa
  
     base_uri 'http://ohanapi.herokuapp.com/api'
 
+    def organizations
+    	self.class.get('/organizations')
+    end
+
+    def search(keyword)
+    	uri = '/search?keyword='+keyword
+    	self.class.get(uri)
+    end
+
   end
 end
--- spec/lib/ohanakapa/wrapper_spec.rb
+++ spec/lib/ohanakapa/wrapper_spec.rb
@@ -1,16 +1,44 @@
 require 'spec_helper'
 describe Ohanakapa::Wrapper do
  
-  describe "default attributes" do
- 
-    it "should include httparty methods" do
-      Ohanakapa::Wrapper.should include(HTTParty)
-    end
- 
-    it "should have the base url set to the Ohana API endpoint" do
-      Ohanakapa::Wrapper.base_uri.should eq('http://ohanapi.herokuapp.com/api')
-    end
- 
+	describe "default attributes" do
+
+		it "should include httparty methods" do
+			Ohanakapa::Wrapper.should include(HTTParty)
+		end
+
+		it "should have the base url set to the Ohana API endpoint" do
+			Ohanakapa::Wrapper.base_uri.should eq('http://ohanapi.herokuapp.com/api')
+		end
+
+	end
+
+	describe "GET organizations" do
+
+ 		let(:wrapper) { Ohanakapa::Wrapper.new }
+
+ 		it "must have an organizations method" do
+			wrapper.should respond_to(:organizations)
+		end
+
+		it "must parse the API response from JSON to Hash" do
+			wrapper.organizations.should be_an_instance_of(Hash)
+		end
+
+ 	end
+
+	describe "GET search" do
+
+	 	let(:wrapper) { Ohanakapa::Wrapper.new }
+
+	 	it "must have a search method" do
+			wrapper.should respond_to(:search)
+		end
+
+		it "must parse the API response from JSON to Hash with 'food' search" do
+			wrapper.search('food').should be_an_instance_of(Hash)
+		end
+
   end
  
 end
