5058b5ef0b638741be9b515e0e1153120a72c4ca
Anselm Bradford <git@anselmbradford.com>
2013-06-15 23:38:28 -0400

    Includes partials in results list
    Makes results list use partials in the ajax directory, so that the
    partials can be refreshed using ajax later on.

--- app/assets/javascripts/modules/organizations/initializer.js
+++ app/assets/javascripts/modules/organizations/initializer.js
@@ -15,6 +15,7 @@ var module = (function (module,window,document,undefined) {
 			module.mapViewManager.init(); // initialize map result view
 			module.resultViewManager.init(); // initialize result list behavior for selecting map or list
 			module.resultSortManager.init(); // initialize result list sorting behavior
+			module.ajax.init(); // initialize ajax	
 	});
 
 return module;
