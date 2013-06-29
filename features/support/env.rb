9a467627143223f1e5994241635349af70afd077
Anselm Bradford <git@anselmbradford.com>
2013-06-28 19:37:56 -0700

    Removes email specs and data
    Removes data json directory and email-spec cucumber tests as they are
    unused.

--- features/support/env.rb
+++ features/support/env.rb
@@ -30,12 +30,12 @@ ActionController::Base.allow_rescue = false
 
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
-begin
-  DatabaseCleaner.orm = 'mongoid'
-  DatabaseCleaner.strategy = :truncation
-rescue NameError
-  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
-end
+#begin
+#  DatabaseCleaner.orm = 'mongoid'
+#  DatabaseCleaner.strategy = :truncation
+#rescue NameError
+#  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+#end
 
 # You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
 # See the DatabaseCleaner documentation for details. Example:
