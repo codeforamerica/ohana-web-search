49639e66872c362224313211ba07b4f0fd69cb98
Anselm Bradford <git@anselmbradford.com>
2013-06-28 19:26:07 -0700

    Removes mongo dependency in travis config

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,3 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.com/rails-environment-variables.html
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.mongo_session['roles'].insert({ :name => role })
-  puts 'role: ' << role
-end
