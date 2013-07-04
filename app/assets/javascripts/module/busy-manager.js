48772a25d2d26a877fc1bf8ee0959c2e32af6062
Anselm Bradford <git@anselmbradford.com>
2013-07-04 01:52:19 -0700

    Refactors busy screen transition

--- app/assets/javascripts/module/busy-manager.js
+++ app/assets/javascripts/module/busy-manager.js
@@ -3,12 +3,14 @@ define(function() {
 	
 	// manages showing and hiding of busy screen
 	var view;
+	var main;
 
 	// init
 	function init()
 	{
 		//console.log(fragment);
 		view = document.getElementById("busy-screen");
+		main = document.getElementById("search-content");
 		hide();
 	}
 
@@ -16,12 +18,18 @@ define(function() {
 	{
 		view.classList.remove('fade-out');
 		view.classList.add('fade-in');
+
+		main.classList.remove('fade-in-delay');
+		main.classList.add('fade-out-delay');
 	}
 	
 	function hide()
 	{
 		view.classList.remove('fade-in');
 		view.classList.add('fade-out');
+
+		main.classList.remove('fade-out-delay');
+		main.classList.add('fade-in-delay');
 	}
 
 	return {
