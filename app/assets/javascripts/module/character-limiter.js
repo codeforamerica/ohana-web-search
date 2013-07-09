ca7f344ff44b04acbd90bee29d8f8aa056e55b97
Anselm Bradford <git@anselmbradford.com>
2013-07-09 16:51:24 -0700

    Fixes #119 - limits length of description field
    Provides a more/less link for showing and hiding more or less of
    description field on the details page.

---
+++ app/assets/javascripts/module/character-limiter.js
@@ -0,0 +1,60 @@
+// manages limiting the characters of the description
+// on the details page and providing a more/less link to 
+// toggle showing or hiding excess text
+define(function() {
+  'use strict';
+
+		// PRIVATE PROPERTIES
+		var desc;
+		var fulltext;
+		var showChar = 400;
+		var ellipsestext = "…";
+		var moretext = "more";
+    var lesstext = "less";
+
+		// PUBLIC METHODS
+		function init()
+		{
+			desc = document.querySelector("#detail-info .description");
+			fulltext = desc.innerHTML;
+				
+				if(fulltext.length > showChar) {
+ 					_showLess(null);
+        }
+		}
+
+		// show more text
+		function _showMore(evt)
+		{
+			var target = evt.target;
+			desc.innerHTML = fulltext;
+
+			var lnk = document.createElement("a");
+			lnk.innerHTML = lesstext;
+       
+      desc.appendChild(lnk);
+      lnk.addEventListener("click", _showLess, false);
+
+      return false;
+		}
+		
+		// show less text
+		function _showLess(evt)
+		{
+      var c = fulltext.substr(0, showChar);
+      var h = fulltext.substr(showChar-1, fulltext.length - showChar);
+			
+			var lnk = document.createElement("a");
+			lnk.innerHTML = moretext;
+
+      var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span><span class="hide">' + h + '</span></span>';
+
+      desc.innerHTML = html;
+      desc.appendChild(lnk);
+      lnk.addEventListener("click", _showMore, false);
+		}
+
+	return {
+		init:init
+	};
+});
--- app/assets/javascripts/routes/organizations/show.js
+++ app/assets/javascripts/routes/organizations/show.js
@@ -1,11 +1,13 @@
 require(['loading-manager', 'popup-manager',
-					'term-popup-manager',
+					'term-popup-manager','character-limiter',
 				 'domReady!'], 
-	function(bm,pm,tpm) {
+	function(bm,pm,tpm,cl) {
   'use strict';
 
   // initialize required modules
   bm.init();
   pm.init();
   tpm.init();
+  cl.init();
+
 });
