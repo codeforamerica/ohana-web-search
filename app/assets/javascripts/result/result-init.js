b22283ec5cabeead230692a6b51e8e768eef0149
Anselm Bradford <git@anselmbradford.com>
2013-07-31 15:28:22 -0700

    Removes unused JavaScript
    Map view has been removed, so this commit removes associated managers.

--- app/assets/javascripts/result/result-init.js
+++ app/assets/javascripts/result/result-init.js
@@ -1,12 +1,12 @@
 // the result and details inits are full modules unlike the other section initializers
 // because they need to have a method to re-initialize their functionality after an 
 // ajax request updates part of the page.
-define(['result/result-view-manager'],function(rvm) {
+define(function() {
   'use strict';
 
   function init()
   {
-		rvm.init();
+		// init any modules needed on the results list. Currently none are needed.
   }
   
   init();
