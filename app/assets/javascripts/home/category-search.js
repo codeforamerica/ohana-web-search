92e2dd198e23aef61c4d2678d7a96fde8195ef6c
Anselm Bradford <git@anselmbradford.com>
2013-08-03 16:12:21 -0700

    Adds web storage for remembering search type selection
    Sets the default search view to the category search and adds web
    storage for remembering last search view selected. Also performs
    styling tweaks.

--- app/assets/javascripts/home/category-search.js
+++ app/assets/javascripts/home/category-search.js
@@ -1,42 +1,69 @@
 // manages limiting the characters of the description
 // on the details page and providing a more/less link to 
 // toggle showing or hiding excess text
-define(function() {
+define(['util/web-storage-proxy'],function(storage) {
   'use strict';
 
 		// PRIVATE PROPERTIES
-		
+		var _searchBox;
+		var _categoryBox;
+
+		var _storageID = "util-web-storage-proxy";
 
 		// PUBLIC METHODS
 		function init()
 		{
-			var searchBox = document.getElementById('search-box');
-			var categoryBox = document.getElementById('category-box');
+			_searchBox = document.getElementById('search-box');
+			_categoryBox = document.getElementById('category-box');
 
 			var catSearch = document.getElementById('category-search');
 			var keywordSearch = document.getElementById('keyword-search');
 
-			catSearch.addEventListener('click',function(){
+			catSearch.addEventListener('click', _categoryClicked, false);
+			keywordSearch.addEventListener('click', _keywordClicked, false);
 
-				searchBox.classList.add('flip');
-				searchBox.classList.remove('unflip');
-
-				categoryBox.classList.add('unflip');
-				categoryBox.classList.remove('flip');
-			});
-
-			keywordSearch.addEventListener('click',function(){
-
-				searchBox.classList.add('unflip');
-				searchBox.classList.remove('flip');
-
-				categoryBox.classList.add('flip');
-				categoryBox.classList.remove('unflip');
-			});
-
-
+			if (storage.getItem(_storageID) == 'KEYWORD')
+			{
+				_showKeywordSearch();
+			}
+			else
+			{
+				_showCategorySearch();
+			}
 		}
 
+		// PRIVATE METHODS
+		function _categoryClicked(evt)
+		{
+			_showCategorySearch();
+		}
+
+		function _keywordClicked(evt)
+		{
+			_showKeywordSearch();
+		}
+
+		function _showKeywordSearch()
+		{
+			_searchBox.classList.add('unflip');
+			_searchBox.classList.remove('flip');
+
+			_categoryBox.classList.add('flip');
+			_categoryBox.classList.remove('unflip');
+
+			storage.setItem(_storageID, 'KEYWORD');
+		}
+
+		function _showCategorySearch()
+		{
+			_searchBox.classList.add('flip');
+			_searchBox.classList.remove('unflip');
+
+			_categoryBox.classList.add('unflip');
+			_categoryBox.classList.remove('flip');
+
+			storage.setItem(_storageID, 'CATEGORY');
+		}
 
 	return {
 		init:init
