4c9e4100adebe74c83a0094ccfe66c69e3ca5e22
Anselm Bradford <git@anselmbradford.com>
2013-08-07 22:49:03 -0700

    Turns off remembering of search selection
    Turns off remembering of search selection till we get the UI worked
    out. Turning it on borks tons of tests with no easy refactor possible
    nor worth it because the UI is changing.

--- app/assets/javascripts/home/category-search.js
+++ app/assets/javascripts/home/category-search.js
@@ -22,13 +22,13 @@ define(['util/web-storage-proxy'],function(storage) {
 			catSearch.addEventListener('click', _categoryClicked, false);
 			keywordSearch.addEventListener('click', _keywordClicked, false);
 
-			if (storage.getItem(_storageID) == 'KEYWORD')
+			if (storage.getItem(_storageID) != 'KEYWORD')
 			{
-				_showKeywordSearch();
+				//_showKeywordSearch();
 			}
 			else
 			{
-				_showCategorySearch();
+				//_showCategorySearch();
 			}
 		}
 
@@ -45,22 +45,18 @@ define(['util/web-storage-proxy'],function(storage) {
 
 		function _showKeywordSearch()
 		{
-			_searchBox.classList.add('fade-in');
-			_searchBox.classList.remove('fade-out');
+			_searchBox.classList.remove('hide');
 
-			_categoryBox.classList.add('fade-out');
-			_categoryBox.classList.remove('fade-in');
+			_categoryBox.classList.add('hide');
 
 			storage.setItem(_storageID, 'KEYWORD');
 		}
 
 		function _showCategorySearch()
 		{
-			_searchBox.classList.add('fade-out');
-			_searchBox.classList.remove('fade-in');
+			_searchBox.classList.add('hide');
 
-			_categoryBox.classList.add('fade-in');
-			_categoryBox.classList.remove('fade-out');
+			_categoryBox.classList.remove('hide');
 
 			storage.setItem(_storageID, 'CATEGORY');
 		}
