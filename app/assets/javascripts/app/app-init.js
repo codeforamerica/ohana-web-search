02ea18e000e7c2857e8f1575ee7b2f9f5a12b975
Anselm Bradford <git@anselmbradford.com>
2013-08-22 16:20:46 -0700

    RequireJS formatting
    Re-arranges RequireJS config file and fixes code comment

--- app/assets/javascripts/app/app-init.js
+++ app/assets/javascripts/app/app-init.js
@@ -1,10 +1,10 @@
-// manages search initialization
+// manages app initialization
 require(['app/loading-manager',
 	'app/popup-manager',
 	'classList',
 	'addEventListener'],function(lm,pm,pfClassList,pfAddEventListener) {
   'use strict';
-	 
+	
 	document.body.classList.add("require-loaded");
 
   lm.hide();
