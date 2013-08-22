8ae336fa8eeba3768133fc70bb6ccd0f41658e5b
Anselm Bradford <git@anselmbradford.com>
2013-08-22 14:03:00 -0700

    Adds try/catch block for IE error suppression

--- app/assets/javascripts/app/app-init.js
+++ app/assets/javascripts/app/app-init.js
@@ -1,8 +1,12 @@
 // manages search initialization
 require(['app/loading-manager','app/popup-manager'],function(lm,pm) {
   'use strict';
-  
-  document.body.classList.add("require-loaded");
+	
+	// try/catch wrap for IE error suppression
+	try
+	{  
+	  document.body.classList.add("require-loaded");
+  }catch(e){}
 
   lm.hide();
   pm.init();
