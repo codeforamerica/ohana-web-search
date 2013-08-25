8f3397c84bf0d018388c4316bec455dfc755f50f
Anselm Bradford <git@anselmbradford.com>
2013-08-25 14:46:56 -0700

    Removes diapers mapping
    Unsure what to map 'diapers' keyword to, so this mapping has been
    removed.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -51,8 +51,6 @@ class OrganizationsController < ApplicationController
         new_params[:keyword] = 'nutrition'
       elsif keyword == 'baby supplies'
         new_params[:keyword] = "UCSF Women's Health Resource Center"
-      elsif keyword == 'diapers'
-        new_params[:keyword] = "UCSF Women's Health Resource Center"
       elsif keyword == 'toys and gifts'
         new_params[:keyword] = 'Community Services Agency of Mountain View'
       elsif keyword == 'addiction & recovery'
