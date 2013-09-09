e3f313a79b021bf9da1ed2408fe5fd90f5817bf3
Anselm Bradford <git@anselmbradford.com>
2013-09-09 15:37:07 -0700

    Sets the timezone to Pacific Time

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,8 @@ module HumanServicesFinder
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
+    config.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -77,5 +79,6 @@ module HumanServicesFinder
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
   end
 end
