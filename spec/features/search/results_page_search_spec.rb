e23259988ce80b12ec5b3a44f807be91ee5b9add
Anselm Bradford <git@anselmbradford.com>
2013-09-20 03:59:58 -0400

    Updates tests and cassettes

--- spec/features/search/results_page_search_spec.rb
+++ spec/features/search/results_page_search_spec.rb
@@ -47,7 +47,7 @@ feature "results page search" do
 
   scenario 'with language that returns less results', :vcr do
     search_by_language("Tagalog (Filipino)")
-    expect(page).to have_content("30 of 107 results")
+    expect(page).to have_content("1-30 of 112 results")
   end
 
 end
