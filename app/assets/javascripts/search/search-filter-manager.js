83b79c63f2af5f9a5b1c84760e1d5be1798d811f
Anselm Bradford <git@anselmbradford.com>
2013-10-14 00:12:03 -0700

    Moves to two-state toggle for search filters

--- app/assets/javascripts/search/search-filter-manager.js
+++ app/assets/javascripts/search/search-filter-manager.js
@@ -3,50 +3,22 @@ define(
 	function() {
   'use strict';
 
-  	var DISABLED = 0;
-  	var ENABLED = 1;
-  	var SOLE_ENABLED = 2;
-
 		function init()
 		{
 			var toggles = document.querySelectorAll('#search-box .search-check');
 			for (var t = 0; t < toggles.length; t++)
 			{
-				toggles[t].addEventListener('mousedown',_toggleClicked,true);
+				toggles[t].addEventListener('mousedown',_toggleClicked,false);
 			}
 		}
 
+		// Makes the full width of the toggle clickable, not just the label.
 		function _toggleClicked(evt)
 		{
-			var toggle = (evt.currentTarget).childNodes[1]; // retrieve first child element
-
-			var offset = (toggle.getAttribute('data-toggle-state')*20) || 0;
-			var quadrant = evt.offsetX + offset;
-
-			console.log( quadrant , offset );
-			// quadrant 3
-			if (quadrant > 40)
+			if (evt.currentTarget == evt.target)
 			{
-				toggle.indeterminate = true;
-				toggle.checked = false;
-				//console.log("toggle SOLE_ENABLED");
-				toggle.setAttribute('data-toggle-state', SOLE_ENABLED);
-			}
-			// quadrant 2
-			else if (quadrant > 20)
-			{
-				toggle.indeterminate = false;
-				toggle.checked = true;
-				//console.log("toggle ENABLED");
-				toggle.setAttribute('data-toggle-state', ENABLED);
-			}
-			// quadrant 1
-			else
-			{
-				toggle.indeterminate = false;
-				toggle.checked = false;
-				//console.log("toggle DISABLED");
-				toggle.setAttribute('data-toggle-state', DISABLED);
+				var toggle = (evt.currentTarget).childNodes[1]; // retrieve first child element
+				toggle.checked = !toggle.checked;
 			}
 			evt.preventDefault();
 			evt.stopPropagation();
