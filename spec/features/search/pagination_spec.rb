b8c943fd42c02759433d03b0a5d83f0ffe1c5a07
Anselm Bradford <git@anselmbradford.com>
2013-11-06 17:15:52 -0800

    Updates tests and cassettes

--- spec/features/search/pagination_spec.rb
+++ spec/features/search/pagination_spec.rb
@@ -37,7 +37,7 @@ feature "results page pagination", :js=>true do
     go_to_page(20)
     expect(page).to have_selector('.pagination')
     expect(page).to have_content('Page: < 1 ... 16 17 18 19 20')
-    expect(page).to have_content('571-589 of 589 results')
+    expect(page).to have_content('571-587 of 587 results')
   end
 
   scenario 'on page less than three pages from beginning of results that have more than five entries', :vcr do
