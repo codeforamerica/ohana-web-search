3b0d48bbbdb37a92928967104d2a413e3bd32f8b
Anselm Bradford <git@anselmbradford.com>
2013-11-07 17:30:06 -0800

    Adds polyfills to about fallback page
    The about page needed polyfills for addEventListener, this commit adds
    that to the dependency list.

--- app/assets/javascripts/routes/about/index.js
+++ app/assets/javascripts/routes/about/index.js
@@ -1,8 +1,9 @@
 require(['app/loading-manager',
 				 'app/feedback-form-manager',
+				 'addEventListener',
 				 'classList',
 				 'domReady!'],
-	function(lm,feedback,classList) {
+	function(lm,feedback,pfAddEventListener,pfClassList) {
   'use strict';
 
 	document.body.classList.add("require-loaded");
