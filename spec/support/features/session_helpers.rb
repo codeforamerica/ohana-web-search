96ed2c4e9f1ff7891129a2d0eeeeb837742fe2d7
Anselm Bradford <git@anselmbradford.com>
2013-11-19 15:01:49 -0800

    Test refactor and cassette updates

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -10,14 +10,14 @@ module Features
         set_location_filter(options)
       end
 
-      find(:css, '#update-btn').click
+      find('#update-btn').click
     end
 
     def search_from_home(options = {})
       visit ("/")
       keyword = options[:keyword]
       fill_in('keyword', :with => keyword)
-      find(:css, '#find-btn').click
+      find('#find-btn').click
     end
 
     def search_for_maceo
@@ -124,78 +124,78 @@ module Features
 
     # navigation helpers
     def visit_details
-      page.find("#list-view").first(:css, 'a').trigger('click')
+      page.find("#list-view").first('a').trigger('click')
     end
 
     def looks_like_results
-      expect(page).to have_content("SanMaceo Example Agency")
-      expect(page).to have_content("1 result")
-      expect(page).to have_title "1 result"
+      page.should have_content("SanMaceo Example Agency")
+      page.should have_content("1 result")
+      page.should have_title "1 result"
     end
 
     def looks_like_puente
-      expect(page).to have_content("Puente Resource Center")
-      expect(page).to have_content("1 result")
-      expect(page).to have_title "1 result"
+      page.should have_content("Puente Resource Center")
+      page.should have_content("1 result")
+      page.should have_title "1 result"
     end
 
     def looks_like_no_results
-      expect(page).to have_selector(".no-results")
-      expect(page).to have_content("your search returned no results.")
-      expect(page).to have_selector("#search-summary")
-      expect(page).to_not have_selector('#map-canvas')
+      page.should have_selector(".no-results")
+      page.should have_content("your search returned no results.")
+      page.should have_selector("#search-summary")
+      page.should_not have_selector('#map-canvas')
     end
 
     def looks_like_location
-      find(:css, "#detail-info .description a").should have_content("more")
-      find(:css, "#detail-info .description a").click
-      find(:css, "#detail-info .description a").should have_content("less")
+      find("#detail-info .description a").should have_content("more")
+      find("#detail-info .description a").click
+      find("#detail-info .description a").should have_content("less")
 
-      expect(page).to have_title "San Maceo Agency | SMC-Connect"
+      page.should have_title "San Maceo Agency | SMC-Connect"
 
-      expect(page).to have_content("Works to control")
-      expect(page).to have_content("Profit and nonprofit")
-      expect(page).to have_content("Marin County")
-      expect(page).to have_content("Walk in")
-      expect(page).to have_content("permits and photocopying")
-      expect(page).to have_content("Russian")
-      expect(page).to have_content("Special parking")
-      expect(page).to have_link("Print")
-      expect(page).to have_link("Directions")
+      page.should have_content("Works to control")
+      page.should have_content("Profit and nonprofit")
+      page.should have_content("Marin County")
+      page.should have_content("Walk in")
+      page.should have_content("permits and photocopying")
+      page.should have_content("Russian")
+      page.should have_content("Special parking")
+      page.should have_link("Print")
+      page.should have_link("Directions")
     end
 
     def looks_like_market
-      find(:css, "#detail-info .description a").should have_content("more")
-      find(:css, "#detail-info .description a").click
-      find(:css, "#detail-info .description a").should have_content("less")
+      find("#detail-info .description a").should have_content("more")
+      find("#detail-info .description a").click
+      find("#detail-info .description a").should have_content("less")
 
-      expect(page).to have_title "San Maceo Agency | SMC-Connect"
+      page.should have_title "San Maceo Agency | SMC-Connect"
 
       within ("#detail-info .payments-accepted") do
         page.should_not have_content("Women, Infants, and Children")
-        find(:css, ".popup-term", :text=>"WIC").trigger(:mousedown)
+        find(".popup-term", :text=>"WIC").trigger(:mousedown)
         page.should have_content("Women, Infants, and Children")
       end
-      expect(page).to have_content("Works to control")
-      expect(page).to have_content("Profit and nonprofit")
-      expect(page).to have_content("Marin County")
-      expect(page).to have_content("Walk in")
-      expect(page).to have_content("permits and photocopying")
-      expect(page).to have_content("Russian")
-      expect(page).to have_content("Special parking")
-      expect(page).to have_link("Print")
-      expect(page).to have_link("Directions")
+      page.should have_content("Works to control")
+      page.should have_content("Profit and nonprofit")
+      page.should have_content("Marin County")
+      page.should have_content("Walk in")
+      page.should have_content("permits and photocopying")
+      page.should have_content("Russian")
+      page.should have_content("Special parking")
+      page.should have_link("Print")
+      page.should have_link("Directions")
     end
 
     def looks_like_homepage
-      expect(page).to have_title "SMC-Connect"
-      expect(page).to have_content "About"
-      expect(page).to have_content "Contribute"
-      expect(page).to have_content "Feedback"
-      expect(page).to have_content "I need"
-      expect(page).to have_content "reporting"
-      expect(page).to have_content "government assistance"
-      expect(page).to_not have_title "1 result"
+      page.should have_title "SMC-Connect"
+      page.should have_content "About"
+      page.should have_content "Contribute"
+      page.should have_content "Feedback"
+      page.should have_content "I need"
+      page.should have_content "reporting"
+      page.should have_content "government assistance"
+      page.should_not have_title "1 result"
       find("#language-box").should have_content("English")
     end
 
