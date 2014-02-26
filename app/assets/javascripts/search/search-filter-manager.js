a0259bfd80868bcb3e74ec63fc8dd7a8b349f9d6
Anselm Bradford <git@anselmbradford.com>
2014-02-26 03:03:30 -0800

    Fixes typo
    Logging branch merged in wrong function name, leading to a bug.

--- app/assets/javascripts/search/search-filter-manager.js
+++ app/assets/javascripts/search/search-filter-manager.js
@@ -12,13 +12,15 @@ define(
   	function init()
   	{
 			// capture form submission
-			var searchForm = document.getElementById("search-form");
-			if (!searchForm) console.log("search form DOM not found!");
-			searchForm.addEventListener("submit",_formSubmitted,false);
+			_searchForm = document.getElementById("search-form");
+			if (!_searchForm) console.log("search form DOM not found!");
+			_searchForm.addEventListener("submit",_formSubmissionHandler,false);
 
-			var resetBtn = document.getElementById("reset-btn-link");
+			// Hook all reset buttons on the page and listen for a click event
+			var resetBtn = document.querySelectorAll(".reset-btn");
 			if (!resetBtn) console.log("reset link DOM not found!");
-			resetBtn.addEventListener("click",_resetClicked,false);
+			for (var r = 0;r<resetBtn.length;r++)
+				resetBtn[r].addEventListener("click",_resetClicked,false);
 
 			// initialize fieldsets
 			var fieldsets = document.querySelectorAll('#search-box fieldset');
