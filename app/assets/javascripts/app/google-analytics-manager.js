402cd9401a67a175a73f8e009ebeb0fcb570d96d
Anselm Bradford <git@anselmbradford.com>
2014-04-04 01:31:32 -0700

    Cleans up JS according to JSHint settings
    Performs general cleanup on JS files according to JSHint settings,
    including adjusting indentation, equality conditions, and adding
    missing semicolons.

--- app/assets/javascripts/app/google-analytics-manager.js
+++ app/assets/javascripts/app/google-analytics-manager.js
@@ -2,32 +2,32 @@
 define(function() {
   'use strict';
 
-		// PUBLIC METHODS
-    // If google analytics variable is initialized it will have a length
-    // greater than 0. If this is the case look for elements on the page
-    // that have the attribute "data-gaq" and add a click event listener
-    // to these elements that register the element with google analytics.
-		function init()
-		{
-      if (_gaq.length > 0)
-      {
-        var toTrack = document.querySelectorAll("*[data-gaq]");
-        var item;
-        for (var t=0; t<toTrack.length; t++)
-        {
-          item = toTrack[t];
-          item.addEventListener("click", _track);
-        }
-      }
-		}
-
-    function _track(evt)
+  // PUBLIC METHODS
+  // If google analytics variable is initialized it will have a length
+  // greater than 0. If this is the case look for elements on the page
+  // that have the attribute "data-gaq" and add a click event listener
+  // to these elements that register the element with google analytics.
+  function init()
+  {
+    if (window._gaq.length > 0)
     {
-      var item = evt.currentTarget;
-      _gaq.push(item.getAttribute("data-gaq"));
+      var toTrack = document.querySelectorAll("*[data-gaq]");
+      var item;
+      for (var t=0; t<toTrack.length; t++)
+      {
+        item = toTrack[t];
+        item.addEventListener("click", _track);
+      }
     }
+  }
 
-	return {
-		init:init
-	};
+  function _track(evt)
+  {
+    var item = evt.currentTarget;
+    window._gaq.push(item.getAttribute("data-gaq"));
+  }
+
+  return {
+    init:init
+  };
 });
--- app/assets/javascripts/app/google-translate-manager.js
+++ app/assets/javascripts/app/google-translate-manager.js
@@ -2,60 +2,60 @@
 define(['util/util'],function(util) {
   'use strict';
 
-		// PRIVATE PROPERTIES
-    var _languages;
-    var _timeoutCount = 0;
+	// PRIVATE PROPERTIES
+  var _languages;
+  var _timeoutCount = 0;
 
-		// PUBLIC METHODS
-		function init()
-		{
-      _deleteTranslateCookies();
-      _checkForGoog();
-		}
+	// PUBLIC METHODS
+	function init()
+	{
+    _deleteTranslateCookies();
+    _checkForGoog();
+	}
 
-    // Removes the google translate cookies by setting their expiration date
-    // into the past.
-    function _deleteTranslateCookies() {
-        var cookies = document.cookie.split(";");
+  // Removes the google translate cookies by setting their expiration date
+  // into the past.
+  function _deleteTranslateCookies() {
+    var cookies = document.cookie.split(";");
 
-        for (var i = 0; i < cookies.length; i++) {
-          var cookie = cookies[i];
-          var eqPos = cookie.indexOf("=");
-          var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-          if (name == "googtrans")
-            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-        }
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      if (name === "googtrans")
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
+  }
 
-    // Polling check for Google Translate drop-down to be initialized
-    function _checkForGoog()
+  // Polling check for Google Translate drop-down to be initialized
+  function _checkForGoog()
+  {
+    // timeout check set to 20 seconds
+    if (_timeoutCount++ < 20)
     {
-      // timeout check set to 20 seconds
-      if (_timeoutCount++ < 20)
-      {
-        setTimeout(function(){
-          _languages = document.querySelector('#google_translate_element select');
-          if (!_languages) _checkForGoog();
-          else _hookDropDown();
-        },1000);
-      }
-      else
-      {
-        console.log("Timeout while initializing Google Translate drop-down!");
-      }
+      setTimeout(function(){
+        _languages = document.querySelector('#google_translate_element select');
+        if (!_languages) _checkForGoog();
+        else _hookDropDown();
+      },1000);
     }
-
-    // Hook in a change event to the language drop-down menu so the page reloads
-    // and is appended a translate query string when the drop-down menu is changed.
-    function _hookDropDown()
+    else
     {
-      _languages.addEventListener('change',_langUpdated,false);
+      console.log("Timeout while initializing Google Translate drop-down!");
     }
+  }
 
-    function _langUpdated(evt)
-    {
-      document.location.search = util.queryString({"translate":_languages.options[_languages.selectedIndex].value});
-    }
+  // Hook in a change event to the language drop-down menu so the page reloads
+  // and is appended a translate query string when the drop-down menu is changed.
+  function _hookDropDown()
+  {
+    _languages.addEventListener('change',_langUpdated,false);
+  }
+
+  function _langUpdated(evt)
+  {
+    document.location.search = util.queryString({"translate":_languages.options[_languages.selectedIndex].value});
+  }
 
 	return {
 		init:init
