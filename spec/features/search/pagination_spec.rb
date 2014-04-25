b09ed77d262477d82d1d3785c426fa9d7985e486
Anselm Bradford <git@anselmbradford.com>
2014-04-25 13:06:01 -0700

    Updates test issues
    - Accidentally committed unresolved conflicts.
    - Regenerates cassettes.
    - Updates Gemfile.lock to latest generated output from Bundle Install.

--- spec/features/search/pagination_spec.rb
+++ spec/features/search/pagination_spec.rb
@@ -1,12 +1,8 @@
 require 'spec_helper'
 
-<<<<<<< HEAD
 # Tests the results page pagination component. This only checks one of the three components on the page,
 # as all should be duplicates of each other.
-feature "results page pagination", :js=>true do
-=======
 feature "results page pagination" do
->>>>>>> master
 
   scenario 'when there are no results', :vcr do
     search_from_home(:keyword => 'asdfg')
@@ -20,52 +16,37 @@ feature "results page pagination" do
     end
   end
 
-<<<<<<< HEAD
-  scenario 'on first page of results that have less than five entries', :vcr do
-    search_from_home(:keyword=>'libraries')
-    within("#floating-results-header .pagination") do
-      expect(page).to have_selector('.next')
-      expect(page).to have_content('12')
-    end
-  end
-
-  scenario 'on last page of results that have less than five entries', :vcr do
-    search_from_home(:keyword=>'libraries')
-    go_to_page(2)
-    within('#floating-results-header .pagination') do
-      expect(page).to have_selector('.prev')
-      expect(page).to have_content('12')
-    end
-=======
   scenario 'on first page with less than 4 pages of results', :vcr do
     search_from_home(:keyword=>'youth')
-    expect(page).to have_selector('.pagination')
-    expect(page).to have_content('Page: 1 2 3 >')
+    within('#floating-results-header .pagination') do
+      expect(page).to have_selector('.next')
+      expect(page).to have_content('123')
+    end
   end
 
   scenario 'on last page with less than 4 pages of results', :vcr do
     search_from_home(:keyword=>'youth')
     go_to_page(3)
-    expect(page).to have_selector('.pagination')
-    expect(page).to have_content('Page: < 1 2 3')
->>>>>>> master
+    within('#floating-results-header .pagination') do
+      expect(page).to have_selector('.prev')
+      expect(page).to have_content('123')
+    end
   end
 
   scenario 'on first page with more than 5 pages of results', :vcr do
     search_from_home
     within('#floating-results-header .pagination') do
       expect(page).to have_selector('.next')
-      expect(page).to have_content('12345…20')
+      expect(page).to have_content('12345…22')
     end
   end
 
   scenario 'on last page with more than 5 pages of results', :vcr do
     search_from_home
-    go_to_page(20)
-    expect(page).to have_content('571-589 of 589 results')
+    go_to_page(22)
     within('#floating-results-header .pagination') do
       expect(page).to have_selector('.prev')
-      expect(page).to have_content('1…1617181920')
+      expect(page).to have_content('1…1819202122')
     end
   end
 
@@ -75,7 +56,7 @@ feature "results page pagination" do
     within('#floating-results-header .pagination') do
       expect(page).to have_selector('.prev')
       expect(page).to have_selector('.next')
-      expect(page).to have_content('12345…20')
+      expect(page).to have_content('12345…22')
     end
   end
 
@@ -85,7 +66,7 @@ feature "results page pagination" do
     within('#floating-results-header .pagination') do
       expect(page).to have_selector('.prev')
       expect(page).to have_selector('.next')
-      expect(page).to have_content('1…23456…20')
+      expect(page).to have_content('1…23456…22')
     end
   end
 
@@ -93,22 +74,21 @@ feature "results page pagination" do
     search_from_home
     go_to_page(22)
     go_to_page(20)
-    go_to_page(18)
     within('#floating-results-header .pagination') do
       expect(page).to have_selector('.prev')
       expect(page).to have_selector('.next')
-      expect(page).to have_content('1…1617181920')
+      expect(page).to have_content('1…1819202122')
     end
   end
 
   scenario 'more than 3 pages out with more than 5 pages of results', :vcr do
     search_from_home
-    go_to_page(20)
-    go_to_page(17)
+    go_to_page(22)
+    go_to_page(18)
     within('#floating-results-header .pagination') do
       expect(page).to have_selector('.prev')
       expect(page).to have_selector('.next')
-      expect(page).to have_content('1…1516171819…20')
+      expect(page).to have_content('1…1617181920…22')
     end
   end
 
