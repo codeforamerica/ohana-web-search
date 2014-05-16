57b2ca9dd11e938c8450e1aa45609c21f1e4882c
Anselm Bradford <git@anselmbradford.com>
2014-05-15 22:50:34 -0700

    Fixes #399 - removes token from travis yml
    Pull requests coming from forks were failing on Travis. This commit:
    - removes the api token from .travis.yml, because it’s not needed for tests
    on Travis.
    - updates cassettes to remove the api_token in the URL call.
    - updates the status_controller_spec to remove the api_token in the URL call.
    - updates the tests that weren't correctly expecting 1 result per page returned.
    - updates the test that had the wrong updated_at time.

--- spec/features/details/location_details_spec.rb
+++ spec/features/details/location_details_spec.rb
@@ -108,7 +108,7 @@ feature "location details" do
       # the time is checked against the Travis CI server time. The time has been
       # removed from the test till this can be sorted.
       #expect(page).to have_content("Tuesday, 1 October 2013 at 3:18 PM")
-      expect(page).to have_content("Friday, 18 April 2014 at")
+      expect(page).to have_content("Friday, 16 May 2014 at")
     end
 
   end
