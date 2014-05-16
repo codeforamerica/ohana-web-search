17d8ed07f7d7e2353304ec3090e4e3b370e3d556
Moncef Belyamani <giter@done>
2014-05-16 11:13:22 -0400

    Disable active_record config in development.rb
    The app is not using ActiveRecord at the moment. If the setting is enabled, the app won't be able to run in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,8 +42,11 @@ HumanServicesFinder::Application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations
-  config.active_record.migration_error = :page_load
+  # If the app is not using active_record/railtie (see application.rb),
+  # the setting below should be disabled.
+  #
+  # Raise an error on page load if there are pending migrations.
+  # config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
