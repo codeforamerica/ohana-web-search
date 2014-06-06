5b9f895ed5e64de73b12215e79dd75443e46fc38
Moncef Belyamani <giter@done>
2014-06-06 17:37:24 -0400

    Temporarily remove GA manager to push to staging
    We removed the GA manager from app-init a few days ago to upgrade to Universal Analytics, and at that time, the GA manager was not called in routes/about/index.js. Now that we've pulled in changes from upstream, it is, but since we removed it, it's causing asset compilation to fail.
    
    So, for now, we're removing the reference to it until we merge PR #450 from upstream.

--- app/assets/javascripts/routes/about/index.js
+++ app/assets/javascripts/routes/about/index.js
@@ -1,17 +1,13 @@
 require([
-  'app/google-analytics-manager',
   'app/google-translate-manager',
   'app/feedback-form-manager',
   'domReady!'
 ],
-  function(ga,gt,feedback) {
+  function(gt, feedback) {
   'use strict';
 
   // This class is added so the tests know the async scripts are loaded.
   document.body.classList.add("require-loaded");
-
-  // Initalize google analytics.
-  ga.init();
 
   // Initialize the google translate.
   gt.init();
