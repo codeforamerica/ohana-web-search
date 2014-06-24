35a667b5cdee544e1d57c8d5afb3c9522e909d7b
Moncef Belyamani <giter@done>
2014-06-24 13:39:19 -0400

    Add Edwin and Effie to feedback email recipients list

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,15 +1,16 @@
 class FeedbackMailer < ActionMailer::Base
-
-  default to: %w[anselm@codeforamerica.org
-              sophia@codeforamerica.org
-              moncef@codeforamerica.org]
+  default to: %w(anselm@codeforamerica.org
+                 sophia@codeforamerica.org
+                 moncef@codeforamerica.org
+                 echan@co.sanmateo.ca.us
+                 everducci@co.sanmateo.ca.us)
 
   # Sends contents of feedback form via email, including user agent
   # @param params [Hash] Email attributes (from, message, user agent)
   # @return [Object] Email object
-  def send_feedback(params={})
+  def send_feedback(params = {})
     message    = params[:message] || '[no message entered]'
-    from       = params[:from].blank? ? "anonymous@none.com" : params[:from]
+    from       = params[:from].blank? ? 'anonymous@none.com' : params[:from]
     user_agent = params[:agent] || '[no user agent recorded]'
 
     subject = "[SMC Connect Feedback] #{from}"
@@ -20,5 +21,4 @@ class FeedbackMailer < ActionMailer::Base
       subject: subject,
       body: body)
   end
-
 end
