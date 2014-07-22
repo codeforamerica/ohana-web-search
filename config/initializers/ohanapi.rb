9adfe131854f01c9341017ce544be83a7f695b24
Moncef Belyamani <giter@done>
2014-07-22 10:02:45 -0400

    Use single-quotes per Ruby style guide.

--- config/initializers/ohanapi.rb
+++ config/initializers/ohanapi.rb
@@ -4,8 +4,8 @@ stack = Faraday::Builder.new do |builder|
   builder.adapter Faraday.default_adapter
 end
 Ohanakapa.configure do |config|
-  config.api_token = ENV["OHANA_API_TOKEN"] if ENV["OHANA_API_TOKEN"].present?
-  config.api_endpoint = ENV["OHANA_API_ENDPOINT"]
+  config.api_token = ENV['OHANA_API_TOKEN'] if ENV['OHANA_API_TOKEN'].present?
+  config.api_endpoint = ENV['OHANA_API_ENDPOINT']
 
   config.middleware = stack
 end
