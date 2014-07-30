0531f89f639adcd2971643994694f9423871e300
Moncef Belyamani <giter@done>
2014-07-30 10:15:01 -0400

    Turn autocomplete back on.
    I think autocomplete is too useful a feature to turn off just for
    cosmetic reasons.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 #
 # Include Internet Explorer polyfills.
-Rails.application.config.assets.precompile << %w(vendor.js ie8.js ie9.js)
+Rails.application.config.assets.precompile += %w(vendor.js ie8.js ie9.js)
