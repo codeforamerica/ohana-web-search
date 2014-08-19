31268cc15ab83708948c9e65fe39c68b6c3244ea
Moncef Belyamani <giter@done>
2014-08-19 15:28:25 -0400

    Cache API responses.
    To improve the performance of Ohana Web Search, and to reduce the load on the API, we tell Ohana Web Search to cache API responses.
    
    This is handled by the Ohanakapa Ruby wrapper in conjuction with the `faraday-http-cache` gem, which determines whether or not to repeat an API request based on the Cache-Control header in the API response.
    
    The amount of time an API response is cached is determined by the API. By default, it is set to 5 minutes, a reasonable amount of time given that the API responses don't change that often.

--- config/initializers/ohanapi.rb
+++ config/initializers/ohanapi.rb
@@ -1,8 +1,11 @@
-stack = Faraday::Builder.new do |builder|
-  builder.use Faraday::HttpCache
+cache_store = ActiveSupport::Cache.lookup_store(:dalli_store)
+
+stack = Faraday::RackBuilder.new do |builder|
+  builder.use Faraday::HttpCache, store: cache_store, logger: Rails.logger
   builder.use Ohanakapa::Response::RaiseError
   builder.adapter Faraday.default_adapter
 end
+
 Ohanakapa.configure do |config|
   config.api_token = ENV['OHANA_API_TOKEN'] if ENV['OHANA_API_TOKEN'].present?
   config.api_endpoint = ENV['OHANA_API_ENDPOINT']
