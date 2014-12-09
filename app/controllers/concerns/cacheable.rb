9ec1c24503854c6cac88dad29658865015b72f94
Moncef Belyamani <giter@done>
2014-12-09 01:14:10 -0500

    Fix alignment in cacheable.rb

--- app/controllers/concerns/cacheable.rb
+++ app/controllers/concerns/cacheable.rb
@@ -2,6 +2,6 @@ module Cacheable
   def cache_page(field)
     return unless ENV['ENABLE_CACHING'] == 'true'
     fresh_when(
-    etag: ENV['ETAG_VERSION_ID'], last_modified: field, public: true)
+      etag: ENV['ETAG_VERSION_ID'], last_modified: field, public: true)
   end
 end
