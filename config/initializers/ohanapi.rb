e76b0e221b5b9b0ff2ba0017f0a422cf38af20ff
Moncef Belyamani <giter@done>
2015-11-28 09:41:19 -0500

    Update redis-rack-cache to latest commit

--- config/initializers/ohanapi.rb
+++ config/initializers/ohanapi.rb
@@ -1,8 +1,8 @@
 cache = Readthis::Cache.new(
-  ENV.fetch('REDISCLOUD_URL', 'redis://localhost:6379'),
-  driver: :hiredis,
   expires_in: ENV.fetch('RRC_EXPIRES_IN', 300).to_i,
-  namespace: 'faraday')
+  namespace: 'faraday',
+  redis: { url: ENV.fetch('REDISCLOUD_URL', 'redis://localhost:6379'), driver: :hiredis }
+)
 
 stack = Faraday::RackBuilder.new do |builder|
   builder.use Faraday::HttpCache, store: cache, serializer: Marshal
