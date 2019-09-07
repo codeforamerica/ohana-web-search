e09beb519456e72f5f2ce19a016d6e2c3d2e17ca
Moncef Belyamani <moncef@example.com>
2019-09-07 17:21:14 -0400

    Add rubocop-rails gem and fix offenses

---
+++ app/models/map_summary.rb
@@ -0,0 +1,33 @@
+class MapSummary
+  def initialize(search)
+    @search = search
+  end
+
+  # rubocop:disable Rails/OutputSafety
+  def call
+    return if map_data.empty?
+
+    return '' if total_map_markers == total_results
+
+    " <i class='fa fa-map-marker'></i> <em>"\
+    "<strong>#{total_map_markers}</strong>/#{total_results} "\
+    "located on map</em>".html_safe
+  end
+  # rubocop:enable Rails/OutputSafety
+
+  private
+
+  attr_reader :search
+
+  def total_results
+    @total_results ||= search.locations.size
+  end
+
+  def total_map_markers
+    @total_map_markers ||= map_data.size
+  end
+
+  def map_data
+    @map_data ||= search.map_data
+  end
+end
