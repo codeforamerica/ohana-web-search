49ec7e8e9873eea5d0d93fd60d5edbf66a209fa7
Moncef Belyamani <gither@fastmail.fm>
2022-04-02 19:32:51 -0400

    Turn on Rails 6 defaults one by one
    There aren't that many that apply to this app. This is one that might.

--- config/initializers/new_framework_defaults_6_0.rb
+++ config/initializers/new_framework_defaults_6_0.rb
@@ -14,7 +14,7 @@
 #
 # This option is not backwards compatible with earlier Rails versions.
 # It's best enabled when your entire app is migrated and stable on 6.0.
-# Rails.application.config.action_dispatch.use_cookies_with_metadata = true
+Rails.application.config.action_dispatch.use_cookies_with_metadata = true
 
 # Change the return value of `ActionDispatch::Response#content_type` to
 # Content-Type header without modification.
@@ -35,8 +35,8 @@
 # Use ActionMailer::MailDeliveryJob for sending parameterized and normal mail.
 #
 # The default delivery jobs (ActionMailer::Parameterized::DeliveryJob, ActionMailer::DeliveryJob),
-# will be removed in Rails 6.1. This setting is not backwards compatible with e
-# arlier Rails versions.
+# will be removed in Rails 6.1. This setting is not backwards compatible with
+# earlier Rails versions.
 # If you send mail in the background, job workers need to have a copy of
 # MailDeliveryJob to ensure all delivery jobs are processed properly.
 # Make sure your entire app is migrated and stable on 6.0 before using this setting.
