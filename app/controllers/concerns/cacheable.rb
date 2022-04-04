f2352143c8d06a10777600bf0643014d2c9a9f6a
Moncef Belyamani <gither@fastmail.fm>
2022-04-03 21:25:46 -0400

    Update Rails from 6.0.4.7 to 6.1.5
    Run `bundle update railties actionmailer`
    Run `script/test`
    Fix deprecation warnings
    Run `rails app:update`

--- app/controllers/concerns/cacheable.rb
+++ app/controllers/concerns/cacheable.rb
@@ -3,7 +3,7 @@ module Cacheable
     return unless ENV['ENABLE_CACHING'] == 'true'
 
     fresh_when(
-      etag: ENV['LATEST_COMMIT_HASH'], last_modified: field, public: true
+      etag: ENV['HEROKU_SLUG_COMMIT'], last_modified: field, public: true
     )
   end
 end
